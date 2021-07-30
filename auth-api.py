980d391aefc1c50626f2dcab0a8d4f4596aacf1f
FoyLiang <Patcharawat.Liang@Gmail.com>
2021-07-30 17:15:23 +0700

    Checking password at API

--- auth-api.py
+++ auth-api.py
@@ -2,6 +2,7 @@ import flask
 from flask import request, jsonify
 from flask_cors import CORS
 import json
+import ast
 
 app = flask.Flask(__name__)
 CORS(app)
@@ -17,8 +18,13 @@ def home():
 def register():
 	print(request.json)
 	recive_data = request.json
+	recive_data = str(recive_data)
 
-	return { "message": "success", "data": recive_data}
+	dict_data = ast.literal_eval(recive_data)
+	if dict_data["password"] != dict_data["password_confirm"]:
+		return { "message": "Error", "data": "Password not match" }, 401
+	else:
+		return { "message": "success", "data": recive_data }
 
 if __name__ == '__main__':
     app.run()
