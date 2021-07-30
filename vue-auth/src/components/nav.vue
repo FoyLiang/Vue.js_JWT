ecd6353e51ed03ef50cfea8d372c130557bc6a45
FoyLiang <Patcharawat.Liang@Gmail.com>
2021-07-30 16:40:27 +0700

    set path in home page

--- vue-auth/src/components/nav.vue
+++ vue-auth/src/components/nav.vue
@@ -1,14 +1,14 @@
 <template>
     <nav class="navbar navbar-expand navbar-light fixed-top">
         <div class="container">
-            <a href="#" class="navbar-brand">Home</a>
+            <a href="/" class="navbar-brand">Home</a>
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a href="#" class="nav-link">Login</a>
+                        <a href="./login" class="nav-link">Login</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">Sing up</a>
+                        <a href="./register" class="nav-link">Sing up</a>
                     </li>
                 </ul>
             </div>
